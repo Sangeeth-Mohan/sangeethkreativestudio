fd274f31894a9144126a6aa6b05d179b07c7be06
Sangee <sangee@Sangees-Latest-MacBook-Pro.local>
2024-07-04 17:54:34 -0700

    fix welcomepage image display issue

--- src/components/WelcomePage/WelcomePageComponent.js
+++ src/components/WelcomePage/WelcomePageComponent.js
@@ -1,13 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { Box, Typography, Button, IconButton, useMediaQuery } from '@mui/material';
-import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
-import { FaFileDownload, FaEnvelope, FaFacebook, FaInstagram, FaLinkedin, FaGithub, FaTwitter } from 'react-icons/fa';
-import Loader from '../Loader/LoaderComponent';
-import welcomeImage from '../../assets/images/SangeethPic1.png';
-import { useThemeContext } from '../../context/ThemeContext';
-import useScreenSize from '../../hooks/useScreenSize';
-import './WelcomePage.css';
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  Typography,
+  Button,
+  IconButton,
+  useMediaQuery,
+} from "@mui/material";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import {
+  FaFileDownload,
+  FaEnvelope,
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaGithub,
+  FaTwitter,
+} from "react-icons/fa";
+import Loader from "../Loader/LoaderComponent";
+import welcomeImage from "../../assets/images/SKM2.png";
+import { useThemeContext } from "../../context/ThemeContext";
+import useScreenSize from "../../hooks/useScreenSize";
+import "./WelcomePage.css";
 
 const WelcomePageComponent = () => {
   const [loading, setLoading] = useState(true);
@@ -24,13 +38,13 @@ const WelcomePageComponent = () => {
   }
 
   const getFontSize = () => {
-    if (width < 600) return 'h4';
-    if (width < 960) return 'h3';
-    return 'h2';
+    if (width < 600) return "h4";
+    if (width < 960) return "h3";
+    return "h2";
   };
 
   const handleDownloadCV = () => {
-    window.open('/SangeethKumarMohanResume.pdf', '_blank');
+    window.open("/SangeethKumarMohanResume.pdf", "_blank");
   };
 
   return (
@@ -43,18 +57,26 @@ const WelcomePageComponent = () => {
       data-theme={mode}
     >
       <Box className="welcome-text-container">
-        <Typography variant={getFontSize()} component="h1" className="name-class">
+        <Typography
+          variant={getFontSize()}
+          component="h1"
+          className="name-class"
+        >
           SANGEETH K MOHAN
         </Typography>
-        <Typography variant="h6" component="p" className='intro-title'>
+        <Typography variant="h6" component="p" className="intro-title">
           Sr. Lead Front-End Engineer
         </Typography>
-        <Typography variant="body1" component="p" className='intro-desc'>
-          Sangeeth has more than 12 years of experience in developing web applications. Renowned for delivering innovative front-end solutions across various industries, including telecommunications, healthcare, energy, and financial services, by leading and collaborating with cross-functional teams.
+        <Typography variant="body1" component="p" className="intro-desc">
+          Sangeeth has more than 12 years of experience in developing web
+          applications. Renowned for delivering innovative front-end solutions
+          across various industries, including telecommunications, healthcare,
+          energy, and financial services, by leading and collaborating with
+          cross-functional teams.
         </Typography>
         <Box display="flex" justifyContent="center" mt={2}>
           <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-          <Button
+            <Button
               variant="contained"
               color="primary"
               startIcon={<FaFileDownload />}
@@ -64,28 +86,62 @@ const WelcomePageComponent = () => {
               Download CV
             </Button>
           </motion.div>
-          <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} mt={2}>
-            <Button variant="contained" color="primary" startIcon={<FaEnvelope />} component={Link} to="/contact">
-             Contact Me
+          <motion.div
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            mt={2}
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={<FaEnvelope />}
+              component={Link}
+              to="/contact"
+            >
+              Contact Me
             </Button>
           </motion.div>
         </Box>
         <Box mt={2} py={3} className="social-icons">
-          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, delay: 0.5 }}>
-            <IconButton href="https://www.linkedin.com/in/sangeethkumarmohan/" target="_blank" className="social-icon">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+          >
+            <IconButton
+              href="https://www.linkedin.com/in/sangeethkumarmohan/"
+              target="_blank"
+              className="social-icon"
+            >
               <FaLinkedin />
             </IconButton>
-            <IconButton href="https://github.com/Sangeeth-Mohan/sangeethkreativestudio" target="_blank" className="social-icon">
-                <FaGithub />
+            <IconButton
+              href="https://github.com/Sangeeth-Mohan/sangeethkreativestudio"
+              target="_blank"
+              className="social-icon"
+            >
+              <FaGithub />
             </IconButton>
-            <IconButton href="https://www.instagram.com/sangee1199" target="_blank" className="social-icon">
-                <FaInstagram />
+            <IconButton
+              href="https://www.instagram.com/sangee1199"
+              target="_blank"
+              className="social-icon"
+            >
+              <FaInstagram />
             </IconButton>
-            <IconButton href="https://twitter.com" target="_blank" className="social-icon">
-                <FaTwitter />
+            <IconButton
+              href="https://twitter.com"
+              target="_blank"
+              className="social-icon"
+            >
+              <FaTwitter />
             </IconButton>
-            <IconButton href="https://www.facebook.com/sangeeth.kumar.524934?mibextid=LQQJ4d" target="_blank" className="social-icon">
-                <FaFacebook />
+            <IconButton
+              href="https://www.facebook.com/sangeeth.kumar.524934?mibextid=LQQJ4d"
+              target="_blank"
+              className="social-icon"
+            >
+              <FaFacebook />
             </IconButton>
           </motion.div>
         </Box>
